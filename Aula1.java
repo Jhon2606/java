b7a09be7f69ce8946892fbee9cfa2ab685d6ea2c
Jhonatas Vinicius Neri Dos Santos <94151809+Jhon2606@users.noreply.github.com>
2024-10-31 18:01:09 -0300

    Create Aula1.java

---
+++ Aula1.java
@@ -0,0 +1,12 @@
+/*public class Classe1{
+    private int x;
+    void fazAlgo(int x){
+        int = 10;
+    }
+    private int x; //Pode pois não é executado sequencialmente linha por linha 
+}*/
+public class Aula1{
+    public static void main(String[] args){
+        System.out.println("Olá, mundo!");
+    }
+}
