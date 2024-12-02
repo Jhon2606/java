575d208d88961ce452d89fa75c7af636cf79f37d
Jhonatas Vinicius Neri Dos Santos <94151809+Jhon2606@users.noreply.github.com>
2024-12-02 17:57:12 -0300

    Create Pessoa2.java

---
+++ listas_exercicios/lista0/lista_2/Pessoa2.java
@@ -0,0 +1,11 @@
+public class Pessoa2{
+    public static void main(String[] args){
+        Robo r1 = new Robo();
+        System.out.println(r1.emitirSom());
+    }
+}
+class Robo{
+        public String emitirSom(){
+            return "Eu sou o Optimus Prime, e deixo essa mensagem.";
+        }
+    }
