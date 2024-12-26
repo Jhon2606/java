392f9ef6d926c8b96e6bbbb182502af765f1d542
Jhonatas Vinicius Neri Dos Santos <94151809+Jhon2606@users.noreply.github.com>
2024-12-26 10:47:44 -0300

    26/12/2024

---
+++ curso_em_video/TesteTipos/src/main/java/com/mycompany/testetipos/TesteTipos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testetipos;
+
+/**
+ *
+ * @author jhon
+ */
+public class TesteTipos {
+
+    public static void main(String[] args) {
+        /*int idade = 30;
+        String valor = Integer.toString(idade);*/
+        String valor = "30";
+        int idade = Integer.parseInt(valor);
+        System.out.println(idade);
+    }
+}
